ef8bb025f7180eb4b7cf9dc8cbafde309b0ffcb1
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-05-26 21:48:54 -0700

    Updated

---
+++ README.md
@@ -0,0 +1,21 @@
+# Lab - Class 401 Python Night
+
+## Project: pythonic-garage-band
+
+### Author: Pedro Perez Alvarez
+
+### Links and Resources : PyCharm, PyTest, GitHub
+
+### Setup : <https://github.com/PGPere/pythonic-garage-band>
+
+### How to run application ?
+
+### run => pyTest
+
+### Describe Tests (copied from assignment)
+
+####  A Band instance should have a name attribute which is a string.
+##### A Band instance should have a members attribute which is a list of instances that inherit from Musician base (or super) class.
+##### A Band instance should have a play_solos method that asks each member musician to play a solo, in the order they were added to band.
+##### A Band instance should have appropriate __str__ and __repr__ methods.
+##### A Band should have a class method to_list which returns a list of previously created Band instances
--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -11,13 +11,34 @@ class Band:
 
 
 class Musician:
-    pass
+    def __init__(self, name, instrument):
+        self.name = name
+        self.instrument = instrument
 
-class Guitarist:
-    pass
+    def __str__(self):
+        return f'My name is {self.name} and I play {self.instrument}'
 
-class Bassist:
-    pass
 
-class Drummer:
-    pass
+class Guitarist(Musician):
+    def __init__(self, name):
+        super().__init__(name, 'guitar')
+
+    def __repr__(self):
+        return f'Guitarist instance. Name = {self.name}'
+
+
+class Bassist(Musician):
+    def __init__(self, name):
+        super().__init__(name, 'bass')
+
+    def __repr__(self):
+        return f'Bassist instance. Name = {self.name}'
+
+
+class Drummer(Musician):
+    def __init__(self, name):
+        super().__init__(name, 'drums')
+
+    def __repr__(self):
+        return f'Drummer instance. Name = {self.name}'
+
